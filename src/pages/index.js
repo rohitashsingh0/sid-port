565aeaa81c394c428af148fe2c9183410ad2e405
rohitashsingh0 <rohitash.singh@gmail.in>
2024-05-23 10:36:21 +0530

    Update Index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,26 @@
 import Layout from 'components/Layout';
-import Banner from 'components/Banner';
-import Aboutus from 'components/Aboutus';
-import Digital from 'components/Digital';
-import Manage from 'components/Manage';
-import Testimonials from 'components/Testimonials';
-import Joinus from 'components/Joinus';
-import Service from 'components/service/page';
+// import Banner from 'components/Banner';
+// import Aboutus from 'components/Aboutus';
+// import Digital from 'components/Digital';
+// import Manage from 'components/Manage';
+// import Testimonials from 'components/Testimonials';
+// import Joinus from 'components/Joinus';
+// import Service from 'components/service/page';
 // import FAQ from 'components/FAQ';
 
 export default function Home() {
   return (
     <Layout>
       <main>
-        <Banner />
+        <h1>Hello World</h1>
+        {/* <Banner />
         <Aboutus />
         <Service />
         <Digital />
         <Manage />
-        {/* <FAQ /> */}
+        <FAQ />
         <Testimonials />
-        <Joinus />
+        <Joinus /> */}
       </main>
     </Layout>
   );
